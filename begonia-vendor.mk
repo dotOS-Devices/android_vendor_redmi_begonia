dc486e01d3f5f3e5325eba6e8f8ffff4ba46f932
SahilSonar <sss.sonar2003@gmail.com>
2021-07-07 09:26:35 +0200

    begonia-vendor: Drop hotword enrollment

--- begonia-vendor.mk
+++ begonia-vendor.mk
@@ -2137,8 +2137,6 @@ PRODUCT_PACKAGES += \
     EngineerMode \
     ImsService \
     SoterService \
-    HotwordEnrollmentOKGoogleCORTEXM4 \
-    HotwordEnrollmentXGoogleCORTEXM4 \
     mediatek-common \
     mediatek-framework \
     mediatek-ims-base \
